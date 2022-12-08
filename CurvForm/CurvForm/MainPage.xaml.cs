997879b9a66eca1dcebc40e485cfa3ac5c7c15fa
Maycol-Zx-PC <alexander51600@hotmail.com>
2022-12-08 16:43:56 -0500

    Check

---
+++ CurvForm/CurvForm/MainPage.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CurvForm
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private string mainContentProperty;
+        private string numeroContentProperty;
+        private string emailContentProperty;
+        private string nacionContentProperty;
+        private DateTime fechaContentProperty;
+        private string nivelIngContentProperty;
+        private string perfilContentProperty;
+
+        public string MainContentProperty
+        {
+            get { return mainContentProperty; }
+            set
+            {
+                mainContentProperty = value;
+                OnPropertyChanged(nameof(MainContentProperty));
+            }
+        }
+        public string perfilContProperty
+        {
+            get { return perfilContentProperty; }
+            set
+            {
+                perfilContentProperty = value;
+                OnPropertyChanged(nameof(perfilContProperty));
+            }
+        }
+        public string nivelIngContProperty
+        {
+            get { return nivelIngContentProperty; }
+            set
+            {
+                nivelIngContentProperty = value;
+                OnPropertyChanged(nameof(nivelIngContProperty));
+            }
+        }
+        public DateTime fechaContPro {
+            get { return fechaContentProperty; }
+            set
+            {
+                fechaContentProperty = value;
+                OnPropertyChanged(nameof(fechaContPro));
+            }
+        }
+        public string nacionContPro
+        {
+            get { return nacionContentProperty; }
+            set
+            {
+                nacionContentProperty = value;
+                OnPropertyChanged(nameof(nacionContPro));
+            }
+        }
+        public string numeroContPro
+        {
+            get { return numeroContentProperty; }
+            set
+            {
+                numeroContentProperty = value;
+                OnPropertyChanged(nameof(numeroContPro));
+            }
+        }
+        public string emailContPro
+        {
+            get { return emailContentProperty; }
+            set
+            {
+                emailContentProperty = value;
+                OnPropertyChanged(nameof(emailContPro));
+            }
+        }
+        public MainPage(string mainContent, string numTelf,string nacion, DateTime fecha, string email,string nivel,string perfil)
+        {
+            InitializeComponent();
+            BindingContext = this;
+            MainContentProperty = mainContent;
+            numeroContPro = numTelf;
+            nacionContPro = nacion;
+            fechaContPro = fecha;
+            emailContPro = email;
+            nivelIngContProperty = nivel;
+            perfilContProperty = perfil;
+        }
+    }
+}
---
+++ CurvForm/CurvForm/MyForm.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CurvForm
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyForm : ContentPage
+    {
+        List<string> NacionPList = new List<string>();
+        public MyForm()
+        {
+            InitializeComponent();
+            //NacionP.Items.Add("Perú");
+            //NacionP.Items.Add("Argentina");
+            //NacionP.Items.Add("Colombia");
+            //NacionP.Items.Add("Brasil");
+            //NacionP.Items.Add("Chile");
+            //NacionP.Items.Add("Ecuador");
+            //NacionP.Items.Add("El Salvador");
+            //NacionP.Items.Add("México");
+            NacionPList.Add("Perú");
+            NacionPList.Add("Argentina");
+            NacionPList.Add("Colombia");
+            NacionPList.Add("Brasil");
+            NacionPList.Add("Chile");
+            NacionPList.Add("Ecuador");
+            NacionPList.Add("El Salvador");
+            NacionPList.Add("México");
+            NacionP.ItemsSource = NacionPList;
+
+            AptitudP.Items.Add("Inteligencia Emocional");
+            AptitudP.Items.Add("Creatividad");
+            AptitudP.Items.Add("Iniciativa");
+            AptitudP.Items.Add("Trabajo en equipo");
+            AptitudP.Items.Add("Resolucion de problemas complejos");
+            AptitudP.Items.Add("Capacidad de liderazgo");
+
+        }
+        
+        void ButtonSubmitClicked(object sender, EventArgs e)
+        {
+            var in_name = name.Text;
+            var in_conTelf = contactoTelf.Text;
+            var in_conEmail = contactoEmail.Text;
+            var in_nacion = NacionPList[0];
+            var in_fecha = startFer.Date;
+            var in_editor = editor.Text;
+                Application.Current.MainPage.Navigation.PushModalAsync(new MainPage(in_name,in_conTelf,in_nacion,in_fecha,in_conEmail,"Nivel de Ingles: Avanzado",in_editor), true);
+            //Application.Current.MainPage.Navigation.PushModalAsync(new TeflCont(input_conTelf), true);
+
+
+        }
+        void ButtonOcupationPlusClicked(System.Object sender, System.EventArgs e)
+        {
+            var entry = new Entry() { Placeholder = "Enter your occupation" };
+            ocupation.Children.Add(entry);
+        }
+    }
+}
